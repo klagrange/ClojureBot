f61d141aefe4f4bd89f71033c9e1730f0582caf6
Baspar <Baspar@users.noreply.github.com>
2017-06-21 15:47:10 +0800

    telegram

--- src/palo_it_bot/telegram.clj
+++ src/palo_it_bot/telegram.clj
@@ -7,8 +7,68 @@
             [clojure.core.async :as a]
             [clojure.pprint :refer [pprint]]))
 
-(defn telegram-in
+;; Token
+(def telegram-token (get-in config/TOKENS [:dev :telegram :TOKEN]))
+
+;; Telegram out multi
+(defmulti telegram-out
+  (fn [payload]
+    (:message-type payload)))
+;; Telegram out text
+(defmethod telegram-out :text
+  [payload]
+  (let [sender-id (payload :sender-id)
+        text (payload :message-value)
+        text (str "You just send me \"" text "\"")]
+    (kvlt/request! {:url (str "https://api.telegram.org/bot" telegram-token "/sendMessage")
+                    :method :post
+                    :headers {:content-type "application/json"}
+                    :type :json
+                    :form {:chat_id sender-id
+                           :text text}})))
+;; Telegram out photo
+(defmethod telegram-out :photo
+  [payload]
+  (let [sender-id (payload :sender-id)
+        photo (:file_id (last (payload :message-value)))]
+    (kvlt/request! {:url (str "https://api.telegram.org/bot" telegram-token "/sendPhoto")
+                    :method :post
+                    :headers {:content-type "application/json"}
+                    :type :json
+                    :form {:chat_id sender-id
+                           :photo photo
+                           :caption "You just send me this picture"}})))
+;; Telegram out unknown
+(defmethod telegram-out :unknown
+  [payload]
+  (let [sender-id (payload :sender-id)
+        text (payload :message-value)
+        text (str "You just send me \"" text "\"")]
+    (kvlt/request! {:url (str "https://api.telegram.org/bot" telegram-token "/sendMessage")
+                    :method :post
+                    :headers {:content-type "application/json"}
+                    :type :json
+                    :form {:chat_id sender-id
+                           :text text}})))
+
+;; Telegram In!
+(defn telegram-in!
   "telegram IN"
   [request respond raise]
-  (let []
-    (respond (ok {:result true}))))
+  (let [params (:params request)
+        sender-id (get-in params [:message :chat :id])
+        text (get-in params [:message :text])
+        photo (get-in params [:message :photo])
+        formatted-message {:sender-id sender-id
+                           :sender-medium :telegram
+                           :message-type (cond
+                                           text :text
+                                           photo :photo
+                                           :else :unknown)
+                           :message-value (or text
+                                              photo)}]
+    (respond (ok {:result true}))
+    (telegram-out formatted-message)))
+(defn telegram-in
+  [request respond raise]
+  (telegram-in! request respond raise))
