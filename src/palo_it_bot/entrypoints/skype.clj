fc93d695e12cf442c4b7d47eb9def43a518b1ab5
Baspar <Baspar@users.noreply.github.com>
2017-06-23 14:36:43 +0800

    [add] Skype pic

--- src/palo_it_bot/entrypoints/skype.clj
+++ src/palo_it_bot/entrypoints/skype.clj
@@ -11,15 +11,32 @@
 ;; Token
 (def skype-token (get-in config/TOKENS [:dev :skype :TOKEN]))
 
-;; Telegram Out!
+
+(defn get-skype-token
+  []
+  (let [skype-public (get-in config/TOKENS [:dev :skype :public])
+        skype-private (get-in config/TOKENS [:dev :skype :private])
+        return (a/<!! (kvlt/request! {:url "https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token"
+                                      :method :post
+                                      :type :x-www-form-urlencoded
+                                      :as :json
+                                      :body (str "grant_type=client_credentials&client_id="
+                                                 skype-public
+                                                 "&client_secret="
+                                                 skype-private
+                                                 "&scope=https%3A%2F%2Fapi.botframework.com%2F.default")}))]
+    (get-in return [:body :access_token])))
+
+;; Skype Out!
 (defmulti skype-out
   (fn [payload]
     (:message-type payload)))
-;; Telegram Out! (text)
+;; Skype Out! (text)
 (defmethod skype-out :text
   [payload]
   (let [sender-id (payload :sender-id)
-        text (payload :message-value)]
+        text (payload :message-value)
+        skype-token (get-skype-token)]
     (kvlt/request! {:url (str "https://apis.skype.com/v3/conversations/" sender-id "/activities")
                     :method :post
                     :headers {:content-type "application/json"
@@ -27,29 +44,27 @@
                     :type :json
                     :form {:type "message/text"
                            :text text}})))
-;; Telegram Out! (photo)
+;; Skype Out! (photo)
 (defmethod skype-out :photo
   [payload]
   (let [sender-id (payload :sender-id)
         photo (:photo (payload :message-value))
-        caption (:caption (payload :message-value))]
+        caption (:caption (payload :message-value))
+        skype-token (get-skype-token)]
     (kvlt/request! {:url (str "https://apis.skype.com/v3/conversations/" sender-id "/activities")
                     :method :post
                     :headers {:content-type "application/json"
                               "Authorization" (str "Bearer " skype-token)}
                     :type :json
                     :form {:type "message/text"
-                           :text caption
-                           ;; :attachments [{:contentType "image/png"
-                           ;;                :contentUrl "http://aka.ms/Fo983c"
-                           ;;                :name "duck-on-a-rock.jpg"}]
-                           }})))
-;; Telegram Out! (unknown)
+                           :text caption}})))
+;; Skype Out! (unknown)
 (defmethod skype-out :unknown
   [payload]
   (let [sender-id (payload :sender-id)
         text (payload :message-value)
-        text (str "You just send me \"" text "\"")]
+        text (str "You just send me \"" text "\"")
+        skype-token (get-skype-token)]
     (kvlt/request! {:url (str "https://apis.skype.com/v3/conversations/" sender-id "/activities")
                     :method :post
                     :headers {:content-type "application/json"
@@ -58,7 +73,7 @@
                     :form {:type "message/text"
                            :text "I don't know"}})))
 
-;; Telegram In!
+;; Skype In!
 (defn skype-in!
   [request respond raise]
   (respond (ok {:result true}))
